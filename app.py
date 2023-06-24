fc8681e31d6e914734883cf46ff11a5b1cf78c67
Vvojciech <pakus94@gmail.com>
2023-06-24 23:20:03 +0200

    Encrypt filenames securely

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 from base64 import b64encode, b64decode
 from flask_cors import CORS
+import uuid
 import os
 import json
 
 app = Flask(__name__)
-CORS(app)  # enable CORS
 api = Api(app)
+CORS(app, resources={r"*": {"origins": "*"}})
+
 
 
 BLOCK_SIZE = 16  # Bytes
@@ -38,11 +40,16 @@ class UploadAPI(Resource):
         filename = file.filename
         key = get_key()
         cipher = AES.new(key, AES.MODE_ECB)
+        encrypted_filename = str(uuid.uuid4())
         encrypted_data = cipher.encrypt(pad(file.read(), BLOCK_SIZE))
-        encrypted_filename = b64encode(cipher.encrypt(pad(bytes(filename, 'utf-8'), BLOCK_SIZE))).decode('utf-8')
+        metadata_filename = encrypted_filename + '.metadata'
+        encrypted_metadata = cipher.encrypt(pad(bytes(filename, 'utf-8'), BLOCK_SIZE))
         
         with open(f'encrypted_files/{encrypted_filename}', 'wb') as enc_file:
             enc_file.write(encrypted_data)
+
+        with open(f'encrypted_files/{metadata_filename}', 'wb') as meta_file:
+            meta_file.write(encrypted_metadata)
 
         return {"message": "File uploaded and encrypted successfully", "file_id": encrypted_filename}, 200
 
@@ -54,11 +61,15 @@ class DownloadAPI(Resource):
         with open(f'encrypted_files/{file_id}', 'rb') as enc_file:
             encrypted_data = enc_file.read()
 
+        with open(f'encrypted_files/{file_id}.metadata', 'rb') as enc_metadata_file:
+            encrypted_metadata = enc_metadata_file.read()
+
         decrypted_data = unpad(cipher.decrypt(encrypted_data), BLOCK_SIZE)
+        decrypted_metadata = unpad(cipher.decrypt(encrypted_metadata), BLOCK_SIZE)
         with open(f'tmp/{file_id}', 'wb') as dec_file:
             dec_file.write(decrypted_data)
 
-        return send_file(f'tmp/{file_id}', as_attachment=True)
+        return send_file(f'tmp/{file_id}', as_attachment=True, download_name=decrypted_metadata.decode())
 
 @app.route('/')
 def redirect_index():
